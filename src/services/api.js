c91efe3bd06f4f0cf6063258322c2b90cc0eb86a
Wladimir Ferreira Gonçalves <codewlad@gmail.com>
2023-07-31 12:32:12 -0300

    Updated api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: "http://localhost:3333"
+    baseURL: "https://codewlad-food-explorer.onrender.com"
 });
 
 /* Use "http://localhost:3333" para acessar o db local */
